9c26da30e81f33342de5a55069be8057dede3bfe
Germán Ruiz del Arco <91187301+germanrda22@users.noreply.github.com>
2022-01-18 12:47:19 +0100

    Delete Segundo_trimestre/proyecto_poo copy directory

--- Segundo_trimestre/proyecto_poo copy/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-spl_autoload_register(function ($clase){
-	$directorio_clase = str_replace('\\', '/', $clase);
-	if (file_exists('controllers/' .$directorio_clase . '.php')){
-		require 'controllers/'.$directorio_clase . '.php';
-	}
-});
