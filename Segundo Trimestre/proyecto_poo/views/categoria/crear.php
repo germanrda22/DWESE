8048c33f2f82f605345f1aae813d554c9150979d
Germán Ruiz del Arco <91187301+germanrda22@users.noreply.github.com>
2022-01-18 08:39:17 +0100

    Delete Segundo Trimestre directory

--- Segundo Trimestre/proyecto_poo/views/categoria/crear.php
+++
@@ -1,7 +0,0 @@
-<h1>Crear Categoria</h1>
-
-<form action="<?=base_url?>categoria/save" method="post">
-	<label for="nombre">Nombre</label>
-	<input type="text" name="nombre" require>
-	<input type="submit" value="Guardar">
-</form>
